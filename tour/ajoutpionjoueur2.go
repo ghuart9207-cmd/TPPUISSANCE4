4112c300b1eb000bb118523a56c63a673dd38d0a
geoffroy <geoffroy.huart@ynov.com>
2025-09-26 12:09:15 +0200

    Fix : Rectification Pion Joueur 2 en O au lieu de X

--- tour/ajoutpionjoueur2.go
+++ tour/ajoutpionjoueur2.go
@@ -10,40 +10,39 @@ func AjouterPionJoueur2(grille variables.Grille) {
 	switch lig {
 	case 1:
 		if col >= 1 && col <= 6 {
-			grille.Ligne1[col] = " X  |"
+			grille.Ligne1[col] = " O  |"
 		} else if col == 7 {
-			grille.Ligne1[7] = " X  "
+			grille.Ligne1[7] = " O  "
 		}
 	case 2:
 		if col >= 1 && col <= 6 {
-			grille.Ligne2[col] = " X  |"
+			grille.Ligne2[col] = " O  |"
 		} else if col == 7 {
-			grille.Ligne2[7] = " X  "
+			grille.Ligne2[7] = " O  "
 		}
 	case 3:
 		if col >= 1 && col <= 6 {
-			grille.Ligne3[col] = " X  |"
+			grille.Ligne3[col] = " O  |"
 		} else if col == 7 {
-			grille.Ligne3[7] = " X  "
+			grille.Ligne3[7] = " O  "
 		}
 	case 4:
 		if col >= 1 && col <= 6 {
-			grille.Ligne4[col] = " X  |"
+			grille.Ligne4[col] = " O  |"
 		} else if col == 7 {
-			grille.Ligne4[7] = " X  "
+			grille.Ligne4[7] = " O  "
 		}
 	case 5:
 		if col >= 1 && col <= 6 {
-			grille.Ligne5[col] = " X  |"
+			grille.Ligne5[col] = " O  |"
 		} else if col == 7 {
-			grille.Ligne5[7] = " X  "
+			grille.Ligne5[7] = " O  "
 		}
 	case 6:
 		if col >= 1 && col <= 6 {
-			grille.Ligne6[col] = " X  |"
+			grille.Ligne6[col] = " O  |"
 		} else if col == 7 {
-			grille.Ligne6[7] = " X  "
+			grille.Ligne6[7] = " O  "
 		}
 	}
-
 }
